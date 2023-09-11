f6f83dba5ae40c9fd1506cce2166bf213b021dd3
Karl-Barce <karlmanuel.barce.cics@ust.edu.ph>
2023-09-11 20:22:38 +0800

    testing

--- Karl_Barce_LabAct/Karl_Barce_LabAct/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Karl_Barce_LabAct.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
