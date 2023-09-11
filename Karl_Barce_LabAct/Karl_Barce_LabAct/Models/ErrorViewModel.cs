061c4d9430882fa9a5b1cf5a7aea36827136b079
Karl-Barce <karlmanuel.barce.cics@ust.edu.ph>
2023-09-11 20:20:51 +0800

    forLabAct

---
+++ Karl_Barce_LabAct/Karl_Barce_LabAct/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Karl_Barce_LabAct.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Karl_Barce_LabAct/Karl_Barce_LabAct/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Karl_Barce_LabAct.Models
+{
+    public enum Course
+    {
+        BSIT, BSCS, BSIS, OTHER
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public double GPA { get; set; }
+        public Course Course { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public string Email { get; set; }
+    }
+}
