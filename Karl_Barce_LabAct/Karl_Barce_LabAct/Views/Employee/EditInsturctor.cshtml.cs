13187da9f7777e3727a55cf35ce6936a7058f4a7
Karl-Barce <karlmanuel.barce.cics@ust.edu.ph>
2023-09-18 11:55:12 +0800

    Elec1C Lab Ac2

---
+++ Karl_Barce_LabAct/Karl_Barce_LabAct/Views/Employee/EditInsturctor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Karl_Barce_LabAct.Views.Employee
+{
+    public class EditInsturctorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
